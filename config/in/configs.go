9a566d118b057a52a4464def0a66fd83d78acb79
YAWAL <gajkaGAJKA140889>
2018-02-09 17:45:16 +0200

    added configs files and structures for them

---
+++ config/in/configs.go
@@ -0,0 +1,23 @@
+package in
+
+type Mongodb struct {
+	Domain  string `json:"domain"`
+	Mongodb bool   `json:"mongodb"`
+	Host    string `json:"host"`
+	Port    string `json:"port"`
+}
+
+type Tsconfig struct {
+	Module    string `json:"module"`
+	Target    string `json:"target"`
+	SourseMap bool   `json:"sourseMap"`
+	Exclude   int    `json:"exclude"`
+}
+
+type TempConfig struct {
+	RestApiRoot    string `json:"restApiRoot"`
+	Host           string `json:"host"`
+	Port           string `json:"port"`
+	Remoting       string `json:"remoting"`
+	LegasyExplorer bool   `json:"legasyExplorer"`
+}
