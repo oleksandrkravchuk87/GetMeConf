81ad290e377f545099a2ed19bb23ba87fda5c6ce
oleksandrkravchuk87 <kravchuk.oleksandr@ukr.net>
2018-02-14 18:33:11 +0200

    added database connection

--- api/getmeconfig.pb.go
+++ api/getmeconfig.pb.go
@@ -8,8 +8,9 @@ It is generated from these files:
 	getmeconfig.proto
 
 It has these top-level messages:
-	ConfigInfo
-	Config
+	GetConfigByNameRequest
+	GetConfigsByTypeRequest
+	GetConfigResponce
 */
 package api
 
@@ -33,40 +34,56 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
-type ConfigInfo struct {
-	ConfigId   string `protobuf:"bytes,1,opt,name=ConfigId" json:"ConfigId,omitempty"`
-	ConfigPath string `protobuf:"bytes,2,opt,name=ConfigPath" json:"ConfigPath,omitempty"`
+type GetConfigByNameRequest struct {
+	ConfigName string `protobuf:"bytes,1,opt,name=ConfigName" json:"ConfigName,omitempty"`
+	ConfigType []byte `protobuf:"bytes,2,opt,name=ConfigType,proto3" json:"ConfigType,omitempty"`
 }
 
-func (m *ConfigInfo) Reset()                    { *m = ConfigInfo{} }
-func (m *ConfigInfo) String() string            { return proto.CompactTextString(m) }
-func (*ConfigInfo) ProtoMessage()               {}
-func (*ConfigInfo) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
+func (m *GetConfigByNameRequest) Reset()                    { *m = GetConfigByNameRequest{} }
+func (m *GetConfigByNameRequest) String() string            { return proto.CompactTextString(m) }
+func (*GetConfigByNameRequest) ProtoMessage()               {}
+func (*GetConfigByNameRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
 
-func (m *ConfigInfo) GetConfigId() string {
+func (m *GetConfigByNameRequest) GetConfigName() string {
 	if m != nil {
-		return m.ConfigId
+		return m.ConfigName
 	}
 	return ""
 }
 
-func (m *ConfigInfo) GetConfigPath() string {
+func (m *GetConfigByNameRequest) GetConfigType() []byte {
 	if m != nil {
-		return m.ConfigPath
+		return m.ConfigType
 	}
-	return ""
+	return nil
 }
 
-type Config struct {
+type GetConfigsByTypeRequest struct {
+	ConfigType []byte `protobuf:"bytes,1,opt,name=ConfigType,proto3" json:"ConfigType,omitempty"`
+}
+
+func (m *GetConfigsByTypeRequest) Reset()                    { *m = GetConfigsByTypeRequest{} }
+func (m *GetConfigsByTypeRequest) String() string            { return proto.CompactTextString(m) }
+func (*GetConfigsByTypeRequest) ProtoMessage()               {}
+func (*GetConfigsByTypeRequest) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
+
+func (m *GetConfigsByTypeRequest) GetConfigType() []byte {
+	if m != nil {
+		return m.ConfigType
+	}
+	return nil
+}
+
+type GetConfigResponce struct {
 	Config []byte `protobuf:"bytes,1,opt,name=Config,proto3" json:"Config,omitempty"`
 }
 
-func (m *Config) Reset()                    { *m = Config{} }
-func (m *Config) String() string            { return proto.CompactTextString(m) }
-func (*Config) ProtoMessage()               {}
-func (*Config) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
+func (m *GetConfigResponce) Reset()                    { *m = GetConfigResponce{} }
+func (m *GetConfigResponce) String() string            { return proto.CompactTextString(m) }
+func (*GetConfigResponce) ProtoMessage()               {}
+func (*GetConfigResponce) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
 
-func (m *Config) GetConfig() []byte {
+func (m *GetConfigResponce) GetConfig() []byte {
 	if m != nil {
 		return m.Config
 	}
@@ -74,8 +91,9 @@ func (m *Config) GetConfig() []byte {
 }
 
 func init() {
-	proto.RegisterType((*ConfigInfo)(nil), "api.ConfigInfo")
-	proto.RegisterType((*Config)(nil), "api.Config")
+	proto.RegisterType((*GetConfigByNameRequest)(nil), "api.GetConfigByNameRequest")
+	proto.RegisterType((*GetConfigsByTypeRequest)(nil), "api.GetConfigsByTypeRequest")
+	proto.RegisterType((*GetConfigResponce)(nil), "api.GetConfigResponce")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -89,8 +107,8 @@ const _ = grpc.SupportPackageIsVersion4
 // Client API for ConfigService service
 
 type ConfigServiceClient interface {
-	SearchConfig(ctx context.Context, in *ConfigInfo, opts ...grpc.CallOption) (*ConfigInfo, error)
-	GetConfig(ctx context.Context, in *ConfigInfo, opts ...grpc.CallOption) (*Config, error)
+	GetConfigByName(ctx context.Context, in *GetConfigByNameRequest, opts ...grpc.CallOption) (*GetConfigResponce, error)
+	GetConfigsByType(ctx context.Context, in *GetConfigsByTypeRequest, opts ...grpc.CallOption) (ConfigService_GetConfigsByTypeClient, error)
 }
 
 type configServiceClient struct {
@@ -101,69 +119,95 @@ func NewConfigServiceClient(cc *grpc.ClientConn) ConfigServiceClient {
 	return &configServiceClient{cc}
 }
 
-func (c *configServiceClient) SearchConfig(ctx context.Context, in *ConfigInfo, opts ...grpc.CallOption) (*ConfigInfo, error) {
-	out := new(ConfigInfo)
-	err := grpc.Invoke(ctx, "/api.ConfigService/SearchConfig", in, out, c.cc, opts...)
+func (c *configServiceClient) GetConfigByName(ctx context.Context, in *GetConfigByNameRequest, opts ...grpc.CallOption) (*GetConfigResponce, error) {
+	out := new(GetConfigResponce)
+	err := grpc.Invoke(ctx, "/api.ConfigService/GetConfigByName", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *configServiceClient) GetConfig(ctx context.Context, in *ConfigInfo, opts ...grpc.CallOption) (*Config, error) {
-	out := new(Config)
-	err := grpc.Invoke(ctx, "/api.ConfigService/GetConfig", in, out, c.cc, opts...)
+func (c *configServiceClient) GetConfigsByType(ctx context.Context, in *GetConfigsByTypeRequest, opts ...grpc.CallOption) (ConfigService_GetConfigsByTypeClient, error) {
+	stream, err := grpc.NewClientStream(ctx, &_ConfigService_serviceDesc.Streams[0], c.cc, "/api.ConfigService/GetConfigsByType", opts...)
 	if err != nil {
 		return nil, err
 	}
-	return out, nil
+	x := &configServiceGetConfigsByTypeClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type ConfigService_GetConfigsByTypeClient interface {
+	Recv() (*GetConfigResponce, error)
+	grpc.ClientStream
+}
+
+type configServiceGetConfigsByTypeClient struct {
+	grpc.ClientStream
+}
+
+func (x *configServiceGetConfigsByTypeClient) Recv() (*GetConfigResponce, error) {
+	m := new(GetConfigResponce)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
 }
 
 // Server API for ConfigService service
 
 type ConfigServiceServer interface {
-	SearchConfig(context.Context, *ConfigInfo) (*ConfigInfo, error)
-	GetConfig(context.Context, *ConfigInfo) (*Config, error)
+	GetConfigByName(context.Context, *GetConfigByNameRequest) (*GetConfigResponce, error)
+	GetConfigsByType(*GetConfigsByTypeRequest, ConfigService_GetConfigsByTypeServer) error
 }
 
 func RegisterConfigServiceServer(s *grpc.Server, srv ConfigServiceServer) {
 	s.RegisterService(&_ConfigService_serviceDesc, srv)
 }
 
-func _ConfigService_SearchConfig_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(ConfigInfo)
+func _ConfigService_GetConfigByName_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetConfigByNameRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ConfigServiceServer).SearchConfig(ctx, in)
+		return srv.(ConfigServiceServer).GetConfigByName(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/api.ConfigService/SearchConfig",
+		FullMethod: "/api.ConfigService/GetConfigByName",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ConfigServiceServer).SearchConfig(ctx, req.(*ConfigInfo))
+		return srv.(ConfigServiceServer).GetConfigByName(ctx, req.(*GetConfigByNameRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _ConfigService_GetConfig_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(ConfigInfo)
-	if err := dec(in); err != nil {
-		return nil, err
+func _ConfigService_GetConfigsByType_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(GetConfigsByTypeRequest)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
 	}
-	if interceptor == nil {
-		return srv.(ConfigServiceServer).GetConfig(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/api.ConfigService/GetConfig",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ConfigServiceServer).GetConfig(ctx, req.(*ConfigInfo))
-	}
-	return interceptor(ctx, in, info, handler)
+	return srv.(ConfigServiceServer).GetConfigsByType(m, &configServiceGetConfigsByTypeServer{stream})
+}
+
+type ConfigService_GetConfigsByTypeServer interface {
+	Send(*GetConfigResponce) error
+	grpc.ServerStream
+}
+
+type configServiceGetConfigsByTypeServer struct {
+	grpc.ServerStream
+}
+
+func (x *configServiceGetConfigsByTypeServer) Send(m *GetConfigResponce) error {
+	return x.ServerStream.SendMsg(m)
 }
 
 var _ConfigService_serviceDesc = grpc.ServiceDesc{
@@ -171,30 +215,35 @@ var _ConfigService_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*ConfigServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "SearchConfig",
-			Handler:    _ConfigService_SearchConfig_Handler,
-		},
-		{
-			MethodName: "GetConfig",
-			Handler:    _ConfigService_GetConfig_Handler,
+			MethodName: "GetConfigByName",
+			Handler:    _ConfigService_GetConfigByName_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "GetConfigsByType",
+			Handler:       _ConfigService_GetConfigsByType_Handler,
+			ServerStreams: true,
+		},
+	},
 	Metadata: "getmeconfig.proto",
 }
 
 func init() { proto.RegisterFile("getmeconfig.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 157 bytes of a gzipped FileDescriptorProto
+	// 201 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x4c, 0x4f, 0x2d, 0xc9,
 	0x4d, 0x4d, 0xce, 0xcf, 0x4b, 0xcb, 0x4c, 0xd7, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x4e,
-	0x2c, 0xc8, 0x54, 0xf2, 0xe0, 0xe2, 0x72, 0x06, 0x0b, 0x7a, 0xe6, 0xa5, 0xe5, 0x0b, 0x49, 0x71,
-	0x71, 0x40, 0x79, 0x29, 0x12, 0x8c, 0x0a, 0x8c, 0x1a, 0x9c, 0x41, 0x70, 0xbe, 0x90, 0x1c, 0x4c,
-	0x65, 0x40, 0x62, 0x49, 0x86, 0x04, 0x13, 0x58, 0x16, 0x49, 0x44, 0x49, 0x81, 0x8b, 0x0d, 0xc2,
-	0x13, 0x12, 0x83, 0xb1, 0xc0, 0x66, 0xf0, 0x04, 0x41, 0x79, 0x46, 0x39, 0x5c, 0xbc, 0x10, 0x56,
-	0x70, 0x6a, 0x51, 0x59, 0x66, 0x72, 0xaa, 0x90, 0x01, 0x17, 0x4f, 0x70, 0x6a, 0x62, 0x51, 0x72,
-	0x06, 0x54, 0x23, 0xbf, 0x5e, 0x62, 0x41, 0xa6, 0x1e, 0xc2, 0x3d, 0x52, 0xe8, 0x02, 0x42, 0x9a,
-	0x5c, 0x9c, 0xee, 0xa9, 0x25, 0xb8, 0x94, 0x73, 0x23, 0x09, 0x24, 0xb1, 0x81, 0x7d, 0x69, 0x0c,
-	0x08, 0x00, 0x00, 0xff, 0xff, 0x6e, 0x78, 0x81, 0xda, 0xfa, 0x00, 0x00, 0x00,
+	0x2c, 0xc8, 0x54, 0x8a, 0xe0, 0x12, 0x73, 0x4f, 0x2d, 0x71, 0x06, 0x8b, 0x3b, 0x55, 0xfa, 0x25,
+	0xe6, 0xa6, 0x06, 0xa5, 0x16, 0x96, 0xa6, 0x16, 0x97, 0x08, 0xc9, 0x71, 0x71, 0x41, 0x84, 0x41,
+	0x82, 0x12, 0x8c, 0x0a, 0x8c, 0x1a, 0x9c, 0x41, 0x48, 0x22, 0x08, 0xf9, 0x90, 0xca, 0x82, 0x54,
+	0x09, 0x26, 0x05, 0x46, 0x0d, 0x9e, 0x20, 0x24, 0x11, 0x25, 0x4b, 0x2e, 0x71, 0xb8, 0xc9, 0xc5,
+	0x4e, 0x95, 0x20, 0x31, 0x0c, 0xa3, 0xc1, 0x5a, 0x19, 0x31, 0xb4, 0x6a, 0x73, 0x09, 0xc2, 0xb5,
+	0x06, 0xa5, 0x16, 0x17, 0xe4, 0xe7, 0x25, 0xa7, 0x0a, 0x89, 0x71, 0xb1, 0x41, 0x44, 0xa0, 0x1a,
+	0xa0, 0x3c, 0xa3, 0xc5, 0x8c, 0x5c, 0xbc, 0x10, 0x66, 0x70, 0x6a, 0x51, 0x59, 0x66, 0x72, 0xaa,
+	0x90, 0x1b, 0x17, 0x3f, 0x9a, 0x9f, 0x84, 0xa4, 0xf5, 0x12, 0x0b, 0x32, 0xf5, 0xb0, 0xfb, 0x54,
+	0x4a, 0x0c, 0x55, 0x12, 0x6e, 0xa3, 0x17, 0x97, 0x00, 0xba, 0x0f, 0x84, 0x64, 0x50, 0xd5, 0xa2,
+	0x7a, 0x0c, 0x97, 0x49, 0x06, 0x8c, 0x49, 0x6c, 0xe0, 0x30, 0x37, 0x06, 0x04, 0x00, 0x00, 0xff,
+	0xff, 0xf4, 0x22, 0x5a, 0xb2, 0x88, 0x01, 0x00, 0x00,
 }
